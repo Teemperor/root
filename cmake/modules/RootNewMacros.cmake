ebee37e52e0e14963da173d4c37f7224d903a737
Raphael Isemann <teemperor@gmail.com>
2017-12-12 15:04:30 +0100

    Also traverse 'used' modules

--- cmake/modules/RootNewMacros.cmake
+++ cmake/modules/RootNewMacros.cmake
@@ -485,6 +485,14 @@ function (ROOT_CXXMODULES_APPEND_TO_MODULEMAP library library_headers)
   set (excluded_headers "${excluded_headers}")
 
   set(modulemap_entry "module \"${library}\" {")
+
+  if ("${library}" STREQUAL Core)
+    set (modulemap_entry "${modulemap_entry}\n  use ROOT_Types\n")
+    set (modulemap_entry "${modulemap_entry}\n  use ROOT_Core_Config_C\n")
+  elseif ("${library}" STREQUAL Thread)
+    set (modulemap_entry "${modulemap_entry}\n  use ThreadLocalStorage\n")
+  endif()
+
   # For modules GCocoa and GQuartz we need objc context.
   if (${library} MATCHES "(GCocoa|GQuartz)")
     set (modulemap_entry "${modulemap_entry}\n  requires objc\n")
